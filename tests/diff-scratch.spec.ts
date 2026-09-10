b23365eee24090ece705ae9d03b8d870ccfc2e2a
Gabriel Miranda <gabrielmfern@outlook.com>
2026-09-10 18:32:29 -0300

    make sure toEqual diffs are test.failing
    Signed-off-by: Gabriel Miranda <gabrielmfern@outlook.com>

--- tests/diff-scratch.spec.ts
+++ tests/diff-scratch.spec.ts
@@ -1,27 +1,27 @@
-test("object", () => {
+test.failing("object", () => {
   expect({ name: "gabriel", port: 3000, tags: ["a", "b"] }).toEqual({ name: "gabriel", port: 3001, tags: ["a", "c", "d"] });
 });
 
-test("string", () => {
+test.failing("string", () => {
   expect("hayllo").toEqual("hello");
 });
 
-test("number", () => {
+test.failing("number", () => {
   expect(1).toEqual(2);
 });
 
-test("array", () => {
+test.failing("array", () => {
   expect([1, 2, 3]).toEqual([1, 3]);
 });
 
-test("nested", () => {
+test.failing("nested", () => {
   expect({ a: { b: { c: [1, { d: "x" }] } } }).toEqual({ a: { b: { c: [1, { d: "y" }] } } });
 });
 
-test("multiline string", () => {
+test.failing("multiline string", () => {
   expect("line 1\nline 2\nline 3").toEqual("line 1\nline two\nline 3");
 });
 
-test("different types", () => {
+test.failing("different types", () => {
   expect({ a: 1 }).toEqual([1]);
 });
